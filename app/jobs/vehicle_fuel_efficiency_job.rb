d538efe3378f0a6f3a33a771eb5729d3b9e1fdf0
Casey Provost <caseyr.provost@gmail.com>
2019-08-08 16:25:25 -0400

    sometimes fuel entries can have zero vlaues

--- app/jobs/vehicle_fuel_efficiency_job.rb
+++ app/jobs/vehicle_fuel_efficiency_job.rb
@@ -10,10 +10,19 @@ class VehicleFuelEfficiencyJob < ApplicationJob
     fuel_entries = Fleetio.client.total_vehicle_fuel_entries(vehicle.fleetio_id)
     gallons_sum = calculate_sum(fuel_entries, "us_gallons")
     mileage_sum = calculate_sum(fuel_entries, "usage_in_mi")
-    vehicle.update!(fuel_efficiency: gallons_sum / mileage_sum)
+
+    if valid_number?(gallons_sum) && valid_number?(mileage_sum)
+
+    end
+      vehicle.update!(fuel_efficiency: gallons_sum / mileage_sum)
+    end
   end
 
   private
+
+  def valid_number?(number)
+    number.present? && number > 0
+  end
 
   def calculate_sum(fuel_entries, attribute)
     fuel_entries.inject(0) do |sum, data|
